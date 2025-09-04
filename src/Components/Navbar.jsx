43df637ee1d215290f484b1ffc9b2f095f336586
Manuela Meupia <manuelameupia4@gmail.com>
2025-09-04 14:59:44 +0100

    Transformation de la navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,31 @@
+import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+
 import './NavbarStyle.css';
 
 function Navbar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const Menu = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <nav className="navStyle">
             <img src="img/logo1.png" alt="" className='logoStyle'/>
-            <p className='welcomeStyle'>
+            <span className='titre'>EvaNails 💅</span>
+            {/* <p className='welcomeStyle'>
                 Bienvenue dans le site web officiel de EvaNails
-            </p>
-            <input type="search" placeholder='Cherchez un modele, une couleur ou une taille...' className='inputSearch'/>
+            </p> */}
+            {/* <input type="search" placeholder='Cherchez un modele, une couleur ou une taille...' className='inputSearch' onClick={BarreRecherche}/> */}
+            <button className="btnMenu" onClick={Menu}>☰</button>
+            <div className={`lienMenu ${isOpen ? 'open' : ''}`}>
+                <Link to="/" className='lienNav'>Accueil</Link>
+                <Link to="/apropos" className='lienNav'>A propos</Link>
+                <Link to="/contacter" className='lienNav'>Nous contacter</Link>
+                <Link to="/" className='lienNav'>Services</Link>
+                <Link to="/" className='lienNav'>Galerie</Link> 
+            </div>       
         </nav>
     )
 }
